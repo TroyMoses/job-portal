ab911843c604e92c8a6cd588447e3342743f72d3
TroyMoses <mugabimoses07@gmail.com>
2024-10-07 01:59:46 +0800

    toast

--- src/components/helpers/ApplyButton.tsx
+++ src/components/helpers/ApplyButton.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const ApplyButton = () => {
+  const applyHandler = () => toast.success("Application Successful");
+
   return (
     <div>
-      <button className="px-10 rounded-lg py-3 bg-blue-600 text-white font-semibold transition-all duration-500 hover:bg-blue-900">
+      <button
+        onClick={applyHandler}
+        className="px-10 rounded-lg py-3 bg-blue-600 text-white font-semibold transition-all duration-500 hover:bg-blue-900"
+      >
         Apply Now
       </button>
     </div>
