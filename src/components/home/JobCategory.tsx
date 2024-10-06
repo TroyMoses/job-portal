7217a3da8d4691f18aff0c054d38103058f4b73a
TroyMoses <mugabimoses07@gmail.com>
2024-10-06 17:23:57 +0800

    heading

--- src/components/home/JobCategory.tsx
+++ src/components/home/JobCategory.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import Heading from "../helpers/Heading";
 
 const JobCategory = () => {
   return (
-    <div>JobCategory</div>
-  )
-}
+    <div className="pt-20 pb-12">
+      <Heading
+        mainHeading="Popular Job Categories"
+        subHeading="2024 Jobs live - 176 Added Today"
+      />
+    </div>
+  );
+};
 
 export default JobCategory;
