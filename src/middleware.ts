327e69b1fb8049fd330ebf912dab61fd1b2cc117
TroyMoses <mugabimoses07@gmail.com>
2024-10-07 02:43:36 +0800

    middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/"],
+  publicRoutes: ["/", "/jobs/alljobs", "/jobs/jobdetails/id"],
 });
 
 export const config = {
