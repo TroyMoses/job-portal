2a95181fe24f11f1d49d9d8007957597f31c06ee
TroyMoses <mugabimoses07@gmail.com>
2024-10-06 16:52:42 +0800

    hero

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import HeroImg from "../../../public/hero.svg";
+import Image from 'next/image';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className="pt-[5rem] pb-[3rem]">
+        <div className='w-[100%] h-[60vh] flex flex-col items-center justify-center'>
+            <div className="w-[80%] mx-auto flex flex-col items-center justify-center gap-[2rem]">
+                {/* Content */}
+                <div className=""></div>
+                {/* Image */}
+                <div className="hidden lg:block">
+                    <Image src={HeroImg} alt="hero image" width={700} height={400} />
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
