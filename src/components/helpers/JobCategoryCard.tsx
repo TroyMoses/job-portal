27a340dbd1e3a0fdeeaf492b5f1162020915cdda
TroyMoses <mugabimoses07@gmail.com>
2024-10-06 17:26:40 +0800

    job category card

---
+++ src/components/helpers/JobCategoryCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const JobCategoryCard = () => {
+  return (
+    <div>JobCategoryCard</div>
+  )
+}
+
+export default JobCategoryCard;
--- src/components/home/JobCategory.tsx
+++ src/components/home/JobCategory.tsx
@@ -8,6 +8,11 @@ const JobCategory = () => {
         mainHeading="Popular Job Categories"
         subHeading="2024 Jobs live - 176 Added Today"
       />
+      <div className="w-[80%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-[4rem] gap-[3rem] items-center">
+        <div>
+          
+        </div>
+      </div>
     </div>
   );
 };
