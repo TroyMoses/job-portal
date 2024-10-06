4b6d7561c1f2e7a987277e44f48b2d3370988aba
TroyMoses <mugabimoses07@gmail.com>
2024-10-06 17:41:05 +0800

    job category card

--- src/components/helpers/Heading.tsx
+++ src/components/helpers/Heading.tsx
@@ -11,7 +11,7 @@ const Heading = ({ mainHeading, subHeading }: Props) => {
         <h1 className="text-black text-[27px] text-opacity-90 font-bold">
             {mainHeading}
         </h1>
-        <p className="mt-[rem] text-[15px] text-gray-800 text-opacity-80 font-medium">
+        <p className="mt-[1rem] text-[15px] text-gray-800 text-opacity-80 font-medium">
             {subHeading}
         </p>
     </div>
--- src/components/helpers/JobCategoryCard.tsx
+++ src/components/helpers/JobCategoryCard.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
-const JobCategoryCard = () => {
-  return (
-    <div>JobCategoryCard</div>
-  )
+interface Props {
+  image: string;
+  category: string;
+  openPositions: string;
 }
+
+const JobCategoryCard = ({ category, image, openPositions }: Props) => {
+  return (
+    <div className="p-4 border-2 cursor-pointer hover:scale-110 hover:shadow-sm transition-all duration-300 border-gray-500 rounded-lg border-opacity-10">
+        <div className="flex items-center space-x-4">
+            {/* Image */}
+            <Image 
+                src={image}
+                alt={category}
+                width={60}
+                height={60}
+            />
+            {/* Content */}
+            
+        </div>
+    </div>
+  );
+};
 
 export default JobCategoryCard;
