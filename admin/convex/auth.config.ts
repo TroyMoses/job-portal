b728ae3cb3bf8aa3d76e31c21e20616ba8ff4175
Daniel Ntege <danientege785@gmail.com>
2024-10-17 01:17:49 +0300

    commit

--- admin/convex/auth.config.ts
+++ admin/convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://calm-mite-8.clerk.accounts.dev",
+      domain: "https://curious-foxhound-25.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
