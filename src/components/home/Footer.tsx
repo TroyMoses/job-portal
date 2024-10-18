a3d68c754f39038c98b71dccc978c44532852423
danielmarv <danientege785@gmail.com>
2024-10-18 19:54:06 +0300

    CCOMMIT

--- src/components/home/Footer.tsx
+++ src/components/home/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className="pt-[1rem] pb-[3rem] bg-[#111111]">
 
       <h1 className="mt-[2rem] text-[14px] w-[80%] text-center mx-auto text-white opacity-50">
-        COPYRIGHT BY TROY LEGACY - 2024
+        COPYRIGHT BY KAKUMIRO DISTRICT - 2024
       </h1>
     </div>
   );
