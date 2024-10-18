b16df3d0cffc44c8f6150e07dea8f9da9e70d701
ottojoash <danientege785@gmail.com>
2024-10-18 12:24:25 +0300

    commit

--- src/components/home/Footer.tsx
+++ src/components/home/Footer.tsx
@@ -1,11 +1,8 @@
 import React from "react";
-import { FaFacebook, FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa6";
 
 const Footer = () => {
   return (
     <div className="pt-[1rem] pb-[3rem] bg-[#111111]">
-      
-          
 
       <h1 className="mt-[2rem] text-[14px] w-[80%] text-center mx-auto text-white opacity-50">
         COPYRIGHT BY TROY LEGACY - 2024
