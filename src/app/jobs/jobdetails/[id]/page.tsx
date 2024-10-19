ab3c593e8c9a7eec5c0f0c04d4f680639a0abcd2
TroyMoses <mugabimoses07@gmail.com>
2024-10-19 19:50:28 +0300

    application for single job

--- src/app/jobs/jobdetails/[id]/page.tsx
+++ src/app/jobs/jobdetails/[id]/page.tsx
@@ -5,16 +5,13 @@ import { useOrganization, useUser } from "@clerk/nextjs";
 import { useQuery } from "convex/react";
 import JobCard from "@/components/helpers/JobCard";
 import { SignInButton, SignedIn, SignedOut } from "@clerk/nextjs";
-import Link from "next/link";
-import React, { useState } from "react";
+import React from "react";
 import { Loader2 } from "lucide-react";
-import { Doc } from "../../../../../convex/_generated/dataModel";
 import { api } from "../../../../../convex/_generated/api";
 
 const JobDetails = ({ params }: { params: { id: string } }) => {
   const organization = useOrganization();
   const user = useUser();
-  const [query, setQuery] = useState("");
 
   let orgId: string | undefined = undefined;
   if (organization.isLoaded && user.isLoaded) {
@@ -53,57 +50,78 @@ const JobDetails = ({ params }: { params: { id: string } }) => {
           </SignInButton>
         </SignedOut>
       </div>
-      <div className="mt-16 w-[80%] mx-auto">
-        <h1 className="text-[20px] font-semibold">Job Purpose</h1>
-        <p className="mt-4 text-black text-opacity-70">{singleJob?.purpose}</p>
+      <div className="mt-5 w-[80%] mx-auto flex flex-col gap-5">
+        <div>
+          <h1 className="text-[25px] uppercase mb-5 font-semibold">
+            Job Details
+          </h1>
+        </div>
 
-        <h1 className="text-[20px] font-semibold">Reports To</h1>
-        <p className="mt-4 text-black text-opacity-70">
-          {singleJob?.reportsTo}
-        </p>
+        <div>
+          <h1 className="text-[20px] font-semibold">Job Purpose</h1>
+          <p className="mt-2 text-black text-opacity-70">
+            {singleJob?.purpose}
+          </p>
+        </div>
 
-        <h1 className="text-[20px] font-semibold">Responsible For</h1>
-        <ul>
-          {singleJob?.responsibleFor?.map((responsibility) => (
-            <li
-              key={responsibility.name}
-              className="mt-4 text-black text-opacity-70"
-            >
-              {responsibility.name}
-            </li>
-          ))}
-        </ul>
+        <div>
+          <h1 className="text-[20px] font-semibold">Reports To</h1>
+          <p className="mt-2 text-black text-opacity-70">
+            {singleJob?.reportsTo}
+          </p>
+        </div>
 
-        <h1 className="text-[20px] font-semibold">Key Outputs</h1>
-        <ul>
-          {singleJob?.keyOutputs?.map((output) => (
-            <li key={output.output} className="mt-4 text-black text-opacity-70">
-              {output.output}
-            </li>
-          ))}
-        </ul>
+        <div>
+          <h1 className="text-[20px] font-semibold">Responsible For</h1>
+          <ul className=" list-inside list-disc">
+            {singleJob?.responsibleFor?.map((responsibility) => (
+              <li
+                key={responsibility.name}
+                className="mt-2 pl-4 text-black text-opacity-70"
+              >
+                {responsibility.name}
+              </li>
+            ))}
+          </ul>
+        </div>
 
-        <h1 className="text-[20px] font-semibold">Key Functions</h1>
-        <ul>
-          {singleJob?.keyFunctions?.map((keyFunction) => (
-            <li
-              key={keyFunction.function}
-              className="mt-4 text-black text-opacity-70"
-            >
-              {keyFunction.function}
-            </li>
-          ))}
-        </ul>
+        <div>
+          <h1 className="text-[20px] font-semibold">Key Outputs</h1>
+          <ul className=" list-inside list-disc">
+            {singleJob?.keyOutputs?.map((output) => (
+              <li
+                key={output.output}
+                className="mt-4 pl-4 text-black text-opacity-70"
+              >
+                {output.output}
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div>
+          <h1 className="text-[20px] font-semibold">Key Functions</h1>
+          <ul className=" list-inside list-disc">
+            {singleJob?.keyFunctions?.map((keyFunction) => (
+              <li
+                key={keyFunction.function}
+                className="mt-2 pl-4 text-black text-opacity-70"
+              >
+                {keyFunction.function}
+              </li>
+            ))}
+          </ul>
+        </div>
 
         <div>
           <h1 className="text-[20px] font-semibold">Person Specifications</h1>
           <div>
             <h1 className="text-[18px] font-semibold">Qualifications</h1>
-            <ul>
+            <ul className=" list-inside list-disc">
               {singleJob?.qualifications?.map((qualification) => (
                 <li
                   key={qualification.qualification}
-                  className="mt-4 text-black text-opacity-70"
+                  className="mt-2 pl-4 text-black text-opacity-70"
                 >
                   {qualification.qualification}
                 </li>
@@ -112,11 +130,11 @@ const JobDetails = ({ params }: { params: { id: string } }) => {
           </div>
           <div>
             <h1 className="text-[18px] font-semibold">Experience</h1>
-            <ul>
+            <ul className=" list-inside list-disc">
               {singleJob?.experiences?.map((experience) => (
                 <li
                   key={experience.experience}
-                  className="mt-4 text-black text-opacity-70"
+                  className="mt-2 pl-4 text-black text-opacity-70"
                 >
                   {experience.experience}
                 </li>
@@ -125,11 +143,11 @@ const JobDetails = ({ params }: { params: { id: string } }) => {
           </div>
           <div>
             <h1 className="text-[18px] font-semibold">Competences</h1>
-            <ul>
+            <ul className=" list-inside list-disc">
               {singleJob?.competences?.map((competence) => (
                 <li
                   key={competence.competence}
-                  className="mt-4 text-black text-opacity-70"
+                  className="mt-2 pl-4 text-black text-opacity-70"
                 >
                   {competence.competence}
                 </li>
