7a3a6d9e5d4b5cad631dd554379ae33aaae4ba9f
TroyMoses <mugabimoses07@gmail.com>
2024-10-19 19:20:53 +0300

    fetched job details

--- src/app/jobs/jobdetails/[id]/page.tsx
+++ src/app/jobs/jobdetails/[id]/page.tsx
@@ -54,30 +54,89 @@ const JobDetails = ({ params }: { params: { id: string } }) => {
         </SignedOut>
       </div>
       <div className="mt-16 w-[80%] mx-auto">
-        <h1 className="text-[20px] font-semibold">Job Description</h1>
+        <h1 className="text-[20px] font-semibold">Job Purpose</h1>
+        <p className="mt-4 text-black text-opacity-70">{singleJob?.purpose}</p>
+
+        <h1 className="text-[20px] font-semibold">Reports To</h1>
         <p className="mt-4 text-black text-opacity-70">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus
-          neque consequuntur vero beatae accusantium cupiditate excepturi ex
-          optio, architecto unde fugit maiores eaque deserunt porro dolorem
-          omnis nobis earum. Cumque.
+          {singleJob?.reportsTo}
         </p>
-        <h1 className="text-[20px] mt-8 font-semibold">Key Responsibilities</h1>
-        <p className="mt-4 text-black text-opacity-70">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus
-          neque consequuntur vero beatae accusantium cupiditate excepturi ex
-          optio, architecto unde fugit maiores eaque deserunt porro dolorem
-          omnis nobis earum. Cumque. Possimus neque consequuntur vero beatae
-          accusantium cupiditate excepturi ex optio, architecto unde fugit
-          maiores eaque deserunt porro dolorem omnis nobis earum. Cumque.
-        </p>
-        <h1 className="text-[20px] mt-8 font-semibold">Skills Required</h1>
-        <ul className="mt-4">
-          <li className="mt-4 text-black text-opacity-70">React JS</li>
-          <li className="mt-4 text-black text-opacity-70">Next JS</li>
-          <li className="mt-4 text-black text-opacity-70">Tailwind CSS</li>
-          <li className="mt-4 text-black text-opacity-70">Typescript</li>
-          <li className="mt-4 text-black text-opacity-70">Next Auth</li>
+
+        <h1 className="text-[20px] font-semibold">Responsible For</h1>
+        <ul>
+          {singleJob?.responsibleFor?.map((responsibility) => (
+            <li
+              key={responsibility.name}
+              className="mt-4 text-black text-opacity-70"
+            >
+              {responsibility.name}
+            </li>
+          ))}
         </ul>
+
+        <h1 className="text-[20px] font-semibold">Key Outputs</h1>
+        <ul>
+          {singleJob?.keyOutputs?.map((output) => (
+            <li key={output.output} className="mt-4 text-black text-opacity-70">
+              {output.output}
+            </li>
+          ))}
+        </ul>
+
+        <h1 className="text-[20px] font-semibold">Key Functions</h1>
+        <ul>
+          {singleJob?.keyFunctions?.map((keyFunction) => (
+            <li
+              key={keyFunction.function}
+              className="mt-4 text-black text-opacity-70"
+            >
+              {keyFunction.function}
+            </li>
+          ))}
+        </ul>
+
+        <div>
+          <h1 className="text-[20px] font-semibold">Person Specifications</h1>
+          <div>
+            <h1 className="text-[18px] font-semibold">Qualifications</h1>
+            <ul>
+              {singleJob?.qualifications?.map((qualification) => (
+                <li
+                  key={qualification.qualification}
+                  className="mt-4 text-black text-opacity-70"
+                >
+                  {qualification.qualification}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <h1 className="text-[18px] font-semibold">Experience</h1>
+            <ul>
+              {singleJob?.experiences?.map((experience) => (
+                <li
+                  key={experience.experience}
+                  className="mt-4 text-black text-opacity-70"
+                >
+                  {experience.experience}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <h1 className="text-[18px] font-semibold">Competences</h1>
+            <ul>
+              {singleJob?.competences?.map((competence) => (
+                <li
+                  key={competence.competence}
+                  className="mt-4 text-black text-opacity-70"
+                >
+                  {competence.competence}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
