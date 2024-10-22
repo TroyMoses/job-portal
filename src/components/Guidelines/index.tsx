70d652df175f75d22ffb21f7f6146f451d0e37a5
Daniel Ntege <danientege785@gmail.com>
2024-10-22 08:56:57 +0300

    commit

--- src/components/Guidelines/index.tsx
+++ src/components/Guidelines/index.tsx
@@ -5,25 +5,22 @@ const ApplicationGuidelines = () => {
     <div className="w-[80%] my-4">
       <ul className="flex flex-col gap-2">
         <li>
-          <strong>1.</strong> Click on the link provided which will the viewer to the Job listing page.
+          <strong>1.</strong> Click <span><a href="/jobs/alljobs">here</a></span>.
         </li>
         <li>
-          <strong>2.</strong> Against each job post, on the list, click on any job/post to view the detailed job composition.
+          <strong>2.</strong> Click on any job/post to view the detailed job composition.
         </li>
         <li>
-          <strong>3.</strong> One is required to register/create an account in order to apply for a job or post he/she is interested in. After registering, an email will be sent to the user with the login link and user credentials.
+          <strong>3.</strong> One is required to register/create an account in order to apply for a job.
         </li>
         <li>
-          <strong>4.</strong> The user will now login.
+          <strong>4.</strong> Click Apply Now.
         </li>
         <li>
-          <strong>5.</strong> For a job post the applicant is interested in, click Apply Now.
+          <strong>6.</strong> The Applicant will be given chance to Review the details supplied before clicking the Submit Button after filling the application form and attaching the all the necessary requirements.
         </li>
         <li>
-          <strong>6.</strong> The user will be given chance to Review the details supplied before clicking the Submit Button after filling the application form and attaching the all the necessary requirements.
-        </li>
-        <li>
-          <strong>7.</strong> The user will have chance to edit in case he/she made mistakes. This should be done before clicking the Submit button.
+          <strong>7.</strong> The Applicant will have chance to edit in case he/she made mistakes. This should be done before clicking the Submit button.
         </li>
         <li>
           <strong>8.</strong> On clicking submit, a list of your submitted applications will be shown on the applicant dashboard.
