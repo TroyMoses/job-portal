33408e344dcc095946d4f2bbb8cb3a3084c29a8a
Troy Moses Mugabi <mugabimoses07@gmail.com>
2024-10-24 06:26:54 +0300

    Update page.tsx

--- src/app/jobs/jobdetails/[id]/page.tsx
+++ src/app/jobs/jobdetails/[id]/page.tsx
@@ -28,15 +28,19 @@ const JobDetails = ({ params }: { params: { id: string } }) => {
         <div className="flex-[0.7]">
           <JobCard job={singleJob!} />
         </div>
+        <SignedIn>
         <ApplyButton
             // @ts-ignore
             id={params.id}
           />
+          </SignedIn>
+        <SignedOut>
           <SignInButton>
             <button className="px-8 py-3 bg-emerald-600 rounded-lg text-white">
               Sign Up To Apply
             </button>
           </SignInButton>
+          </SignedOut>
       </div>
       <div className="mt-5 w-[80%] mx-auto flex flex-col gap-5">
         <div>
